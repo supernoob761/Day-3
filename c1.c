39c31ca666d88c8d53b21881fbc5adb530cb6e04
Super Noob <supernoob761@gmail.com>
2025-09-04 11:26:45 +0100

    a begining

---
+++ c1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int total;
+
+ for(int i=10 ; i>0 ; i--) {
+    total = 5 * i;
+    printf("5 * %d = %d\n",i , total);
+ }
+    return 0;
+}
